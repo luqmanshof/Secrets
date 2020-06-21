0c4c701ac7c685cd2e670635ed91431ed5c7e42c
Luqman Shofuleji <luqmanshof@gmail.com>
2020-06-21 15:45:39 +0100

    Finishing up

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);   // Setup user schema to use passportLocalMongoose as plugin
@@ -105,17 +106,35 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+    User.find({ "secret": { $ne: null } }, function (err, founUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (founUsers) {
+                res.render("secrets", { usersWithSecrets: founUsers });
+            }
+        }
+    });
+
+    // if (req.isAuthenticated()) {
+    //     res.render("secrets");
+    // } else {
+    //     res.redirect("/login");
+    // }
 
 });
 
 app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
+});
+
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.get("/auth/google",
@@ -171,6 +190,23 @@ app.post("/login", function (req, res) {
         }
     })
 
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 })
 
 
